2a20ac8319d4a18bddfb82f0a4a84a855d89b76c
Pablo Orellana <hola@mendi.cl>
2026-01-19 10:50:29 +0100

    feat: add inertia method with JSON rendering
    Add the main inertia() method that renders JSON responses for Inertia XHR
    requests. Includes inertia_shared_data (empty hash default) and
    inertia_version helper that supports both static values and callables.

--- lib/roda/plugins/inertia.rb
+++ lib/roda/plugins/inertia.rb
@@ -17,6 +17,35 @@ class Roda
         def inertia_request?
           request.get_header('HTTP_X_INERTIA') == 'true'
         end
+
+        def inertia(component, props: {})
+          page_data = {
+            component: component,
+            props: inertia_shared_data.merge(props),
+            url: request.url,
+            version: inertia_version
+          }
+
+          if inertia_request?
+            response['Content-Type'] = 'application/json'
+            response['X-Inertia'] = 'true'
+            page_data.to_json
+          else
+            # HTML rendering - next task
+            page_data.to_json
+          end
+        end
+
+        def inertia_shared_data
+          {}
+        end
+
+        private
+
+        def inertia_version
+          version = opts[:inertia_version]
+          version.respond_to?(:call) ? version.call : version
+        end
       end
     end
 
---
+++ test/inertia_render_json_test.rb
@@ -0,0 +1,43 @@
+# test/inertia_render_json_test.rb
+require_relative 'test_helper'
+require 'json'
+
+class InertiaRenderJsonTest < InertiaTest
+  def app
+    Class.new(Roda) do
+      plugin :inertia, version: '1.0'
+
+      route do |r|
+        r.root do
+          inertia 'Home', props: { name: 'World' }
+        end
+      end
+    end
+  end
+
+  def test_returns_json_for_inertia_request
+    header 'X-Inertia', 'true'
+    get '/'
+
+    assert_equal 'application/json', last_response.content_type
+    assert_equal 'true', last_response['X-Inertia']
+  end
+
+  def test_json_contains_component_and_props
+    header 'X-Inertia', 'true'
+    get '/'
+
+    data = JSON.parse(last_response.body)
+    assert_equal 'Home', data['component']
+    assert_equal({ 'name' => 'World' }, data['props'])
+  end
+
+  def test_json_contains_url_and_version
+    header 'X-Inertia', 'true'
+    get 'http://example.org/'
+
+    data = JSON.parse(last_response.body)
+    assert_equal 'http://example.org/', data['url']
+    assert_equal '1.0', data['version']
+  end
+end
